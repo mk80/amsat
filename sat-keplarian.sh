c95f1d48158fa184b6a11c3b6977878e1aee0470
mk80 <michaelkosko@gmail.com>
2020-08-19 20:05:57 -0700

    added some notes and add resource for tle

--- sat-keplarian.sh
+++ sat-keplarian.sh
@@ -4,4 +4,7 @@ timestamp=$(date +"%d%m%y-%H%M%S")
 
 filename="keps-${timestamp}.txt"
 
-curl https://www.amsat.org/tle/current/nasabare.txt > ${filename}
+# different sources for TLE
+
+#curl https://www.amsat.org/tle/current/nasabare.txt > ${filename}
+curl https://celestrak.com/NORAD/elements/amateur.txt > ${filename}
