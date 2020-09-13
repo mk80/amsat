4d14713e13ba9e42835c0685a3c817958619165e
mk80 <michaelkosko@gmail.com>
2020-09-13 00:05:59 -0700

    process all satellites passing per day

--- amsat.py
+++ amsat.py
@@ -1,18 +1,28 @@
 from skyfield.api import Topos, load, EarthSatellite
+from datetime import datetime
+from pytz import timezone
 
-stations_url = 'http://celestrak.com/NORAD/elements/amateur.txt'
+#stations_url = 'http://celestrak.com/NORAD/elements/amateur.txt'
+stations_url = 'https://www.amsat.org/tle/current/nasabare.txt'
 satellites = load.tle_file(stations_url)
 print('Loaded', len(satellites), 'satellites')
 
 ts = load.timescale()
 
 home = Topos('32.4809 N', '117.2250 W')
-t0 = ts.utc(2020, 9, 13)
-t1 = ts.utc(2020, 9, 14)
+
+print(datetime.today())
+today = datetime.today()
+print(today.year)
+print(today.month)
+print(today.day)
+
+t0 = ts.utc(today.year, today.month, today.day)
+t1 = ts.utc(today.year, today.month, today.day + 1)
 
 for sat in satellites:
     print(sat)
-    t, events = sat.find_events(home, t0, t1, altitude_degrees=1.0)
+    t, events = sat.find_events(home, t0, t1, altitude_degrees=30.0)
     for ti, event in zip(t, events):
-        name = ('rise above 1°', 'culminate', 'set below 1°')[event]
+        name = ('rise above 30°', 'culminate', 'set below 30°')[event]
         print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
